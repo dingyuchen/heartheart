1eff785e60500d6dae57a53e34846d8ec5a34bb4
Branson <branson.ng@u.nus.edu>
2019-05-18 23:01:27 +0800

    FIXED THE BUG

--- App.js
+++ App.js
@@ -5,60 +5,63 @@ import styles from "./styles";
 import Welcome from "./src/screens/Welcome.js";
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            first_time: false,
-            message: "Start Recording",
-            isRecording: false,
-            debug: "default state"
-        };
-    }
-
-    onPress = () => {
-        const { isRecording } = this.state;
-        console.log(isRecording);
-        this.setState({
-            debug: "onPress activated",
-            isRecording: !isRecording
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_time: false,
+      message: "Start Recording",
+      isRecording: false,
+      debug: "default state"
     };
+  }
 
-    onPressIn = () => {
-        const { isRecording } = this.state;
-        this.setState({
-            message: "Recording.........",
-            isRecording: !isRecording,
-            debug: "onPressIn activated"
-        });
-    };
+  onPress = () => {
+    const { isRecording } = this.state;
+    console.log(isRecording);
+    this.setState({
+      debug: "onPress activated",
+      isRecording: !isRecording
+    });
+  };
 
-    onPressOut = () => {
-        const { isRecording } = this.state;
-        this.setState({
-            message: "Recording stopped",
-            isRecording: !isRecording,
-            debug: "onPressOut activated"
-        });
-    };
+  onPressIn = () => {
+    const { isRecording } = this.state;
+    this.setState({
+      message: "Recording.........",
+      isRecording: !isRecording,
+      debug: "onPressIn activated"
+    });
+  };
 
-    render() {
-        const { first_time, message, debug } = this.state;
-        const imgSrc = isRecording ? "./assets/clicked_heart.png" : "./assets/heart.png";
-        return (
-            <View style={styles.container}>
-                {/* first line is hidden under status bar due branson's tricky screen */}
-                {first_time ? <Welcome /> : null}
-                <TouchableOpacity
-                    style={styles.recordButton}
-                    onPressIn={this.onPressIn}
-                    onPressOut={this.onPressOut}
-                >
-                    <Image source={require(imgSrc)}/>
-                    <Text>{message} </Text>
-                </TouchableOpacity>
-                <Text style={styles.text}>{debug}</Text>
-            </View>
-        );
-    }
+  onPressOut = () => {
+    const { isRecording } = this.state;
+    this.setState({
+      message: "Recording stopped",
+      isRecording: !isRecording,
+      debug: "onPressOut activated"
+    });
+  };
+
+  render() {
+    const { first_time, message, debug, isRecording } = this.state;
+    return (
+      <View style={styles.container}>
+        {/* first line is hidden under status bar due branson's tricky screen */}
+        {first_time ? <Welcome /> : null}
+        <TouchableOpacity
+          style={styles.recordButton}
+          onPressIn={this.onPressIn}
+          onPressOut={this.onPressOut}
+        >
+          {isRecording ? (
+            <Image source={require("./assets/clicked_heart.png")} />
+          ) : (
+            <Image source={require("./assets/heart.png")} />
+          )}
+          <Text>{message} </Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>{debug}</Text>
+      </View>
+    );
+  }
 }
