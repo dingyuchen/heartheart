42e9269062a7f0ad24b816d92fc44ab43918f01b
Tianyi <42670338+tysng@users.noreply.github.com>
2019-05-18 22:41:24 +0800

    fix welcome view style.js import

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../../styles";
 
 export default class Welcome extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>This should be the tutorial to show how to use the app</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This should be the tutorial to show how to use the app</Text>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
